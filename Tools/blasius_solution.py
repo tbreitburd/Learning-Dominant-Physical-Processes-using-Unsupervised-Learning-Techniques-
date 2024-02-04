242c1683eb5fcf61b84cb2b05a6cd120e98ea6d8
Thomas Breitburd <tmb76@cam.ac.uk>
2024-02-04 16:53:54 +0000

    Fixed issue with the bc_fn function definition and missing arguments
    Added a global eta variable to the blasius_solution.py file to fix missing arguments issues

--- Tools/blasius_solution.py
+++ Tools/blasius_solution.py
@@ -2,6 +2,13 @@ import numpy as np
 from scipy.integrate import odeint
 
 
+eta_inf = 200
+# Arbitrary "infinite" upper limit for domain
+d_eta = 0.01
+# Step size
+eta = np.arange(0, eta_inf, d_eta)
+
+
 def blasius_rhs(f):
     """RHS of Blasius equation recast as first order nonlinear ODE
     f[0] = f
@@ -13,7 +20,9 @@ def blasius_rhs(f):
 
 def bc_fn(f0):
     """Solve with unknown initial condition as guess and evaluate at upper boundary"""
+
     global eta
+
     f = odeint(lambda f, t: blasius_rhs(f), f0, eta)
     # return discrepancy between upper boundary and desired f[2] = 1
     return [f0[0], f0[1], f[-1, 1] - 1]
