5cafa0496b4f09f38c8a4dd978f1b219bb09852e
Thomas Breitburd <tmb76@cam.ac.uk>
2024-05-13 18:13:08 +0100

    Created and wrote weighted_k_means script
    Made script version of weighted K_means notebook.

---
+++ Scripts/weighted_k_means.py
@@ -0,0 +1,332 @@
+# ---------------------------------------
+# Import modules
+# ---------------------------------------
+
+import numpy as np
+import h5py
+import sys
+from sklearn.cluster import KMeans
+from sklearn.decomposition import SparsePCA
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+# adding Tools to the system path
+sys.path.insert(0, "../Tools/")
+import plot_funcs as pf  # noqa: E402
+import preprocessing as pp  # noqa: E402
+
+# ---------------------------------------
+# Preprocessing
+# ---------------------------------------
+
+# Load the data from http://turbulence.pha.jhu.edu/Transition_bl.aspx
+file = h5py.File("../Data/Transition_BL_Time_Averaged_Profiles.h5", "r")
+
+# Get arrays for variables and the Reynold's averages
+x = np.array(file["x_coor"])
+y = np.array(file["y_coor"])
+u = np.array(file["um"])
+v = np.array(file["vm"])
+p = np.array(file["pm"])
+Ruu = np.array(file["uum"]) - u**2
+Ruv = np.array(file["uvm"]) - u * v
+Rvv = np.array(file["uvm"]) - v**2
+
+# Visualize by wall-normal Reynolds stress
+X, Y = np.meshgrid(x, y)
+
+# Include line of 99% of free flow mean velocity
+# Values from http://turbulence.pha.jhu.edu/docs/README-transition_bl.pdf
+U_inf = 1
+nu = 1 / 800
+Re = (U_inf / nu) * x
+
+
+# ------- Get the derivatives --------
+print("----------------------------------")
+print("Getting derivatives")
+print("----------------------------------")
+
+# Get space steps
+dx = x[1] - x[0]
+dy = y[1:] - y[:-1]
+
+nx = len(x)  # Number of points in x
+ny = len(y)  # Number of points in y
+
+# Compute the derivatives
+Dx, Dy = pp.get_derivatives(nx, ny, dx, dy)
+
+# Get double derivatives
+Dxx = 2 * (Dx @ Dx)
+Dyy = 2 * (Dy @ Dy)
+
+# Flatten arrays for matrix multiplication, using fortran ordering
+u = u.flatten("F")
+v = v.flatten("F")
+p = p.flatten("F")
+Ruu = Ruu.flatten("F")
+Ruv = Ruv.flatten("F")
+
+
+# Get derivatives
+ux = Dx @ u
+uy = Dy @ u
+vx = Dx @ v
+vy = Dy @ v
+px = Dx @ p
+py = Dy @ p
+lap_u = (Dxx + Dyy) @ u
+Ruux = Dx @ Ruu
+Ruvy = Dy @ Ruv
+
+# Labels of terms in the RANS equation
+labels = [
+    r"$\bar{u} \bar{u}_x$",
+    r"$\bar{v}\bar{u}_y$",
+    r"$\rho^{-1} \bar{p}_x$",
+    r"$\nu \nabla^2 \bar{u}$",
+    r"$\overline{(u^\prime v^\prime)}_y$",
+    r"$\overline{({u^\prime} ^2)}_x$",
+]
+
+# ---------------------------------------------
+# Obtain the equation space representation
+# of the RANS equation
+# ---------------------------------------------
+
+# Gather the terms into an array of features
+features = 1e3 * np.vstack([u * ux, v * uy, px, nu * lap_u, Ruvy, Ruux]).T
+nfeatures = features.shape[1]
+
+# ---------------------------------------------
+# Visualise the weights
+# ---------------------------------------------
+
+ox = np.linspace(-10, 10, 100)
+plt.plot(ox, 1 - (np.tanh((1 / 2) * ox)) ** 2)
+plt.savefig("../Plots/tanh_weights.png")
+plt.close()
+
+# ---------------------------------------------
+# Cluster using weighted K-Means
+# ---------------------------------------------
+print("----------------------------------")
+print("Clustering with weighted K-Means")
+print("----------------------------------")
+
+# Fit weighted k-means model
+# Fit weighted k-means model
+nc = 6  # Number of clusters
+seed = 75016  # Set a seed for debugging/plotting
+np.random.seed(seed)
+
+model = KMeans(n_clusters=nc, n_init=10, random_state=seed)
+
+# Train on only a subset (20%) of the data
+sample_pct = 0.2
+mask = np.random.permutation(features.shape[0])[: int(sample_pct * features.shape[0])]
+
+# Define the sample weights using a tanh function of the distance to the origin
+# in the feature space
+origin_dist_mask = np.linalg.norm(features[mask, :], axis=1)
+sample_weights_mask = 1 - (np.tanh((1) * origin_dist_mask)) ** 2
+
+# Fit the model
+model.fit(features[mask, :], sample_weight=sample_weights_mask)
+
+# Predict clusters for all data
+origin_dist = np.linalg.norm(features, axis=1)
+sample_weights = 1 - (np.tanh((1) * origin_dist)) ** 2
+clustering = model.predict(features, sample_weight=sample_weights)
+
+# Get covariances in each cluster
+covs = np.zeros((nc, nfeatures, nfeatures))
+for i in range(nc):
+    mask_ = clustering == i
+    covs[i, :, :] = np.cov(features[mask_, :].T)
+# Plot the covariance matrices between terms for each of the weighted K-Means cluster
+pf.plot_cov_mat(covs, nfeatures, nc, "Other", False)
+
+# ---------------------------------------------
+# Cluster the data and visualise:
+# equation space and physical space
+# ---------------------------------------------
+
+# Predict the cluster index for each data point
+cluster_idx = clustering + 1
+
+# Plot the clusters in equation space with 2D projections
+pf.plot_clustering_2d_eq_space(features[mask, :], cluster_idx[mask], nc, False)
+
+# Assign points in space to each cluster
+cluster_idx = clustering
+clustermap = np.reshape(cluster_idx, [ny, nx], order="F")
+
+# Visualize the clustering in space
+pf.plot_clustering_space(clustermap, x, y, X, Y, nx, ny, nc, u, U_inf, False)
+
+# ---------------------------------------------
+# Sparse Principal Component Analysis (SPCA)
+# dimensionality reduction
+# ---------------------------------------------
+
+print("----------------------------------")
+print("Applying Sparse PCA")
+print("----------------------------------")
+# Sparse PCA to identify directions of nonzero variance in each cluster
+
+alphas = [1e-4, 1e-3, 1e-2, 0.1, 1, 10, 100, 1e3, 1e4, 1e5]
+err = np.zeros([len(alphas)])
+sparsity = np.zeros([len(alphas)])
+
+for k in range(len(alphas)):
+    for i in range(nc):
+        # Identify points in the field corresponding to each cluster
+        feature_idx = np.nonzero(cluster_idx == i)[0]
+        cluster_features = features[feature_idx, :]
+
+        # Conduct Sparse PCA
+        spca = SparsePCA(
+            n_components=1, alpha=alphas[k], random_state=seed
+        )  # normalize_components=True
+        spca.fit(cluster_features)
+
+        # Identify active and terms
+        active_terms = np.nonzero(spca.components_[0])[0]
+        inactive_terms = [feat for feat in range(nfeatures) if feat not in active_terms]
+
+        # Calculate the error, as the sum of the norms of the inactive terms
+        err[k] += np.linalg.norm(cluster_features[:, inactive_terms])
+
+pf.plot_spca_residuals(alphas, err, False)
+
+
+# Now with optimal alpha, get the active terms in each cluster
+alpha_opt = 8  # Optimal alpha value
+
+spca_model = np.zeros([nc, nfeatures])  # Store the active terms for each cluster
+
+for i in range(nc):
+    feature_idx = np.nonzero(cluster_idx == i)[0]
+    cluster_features = features[feature_idx, :]
+
+    spca = SparsePCA(
+        n_components=1, alpha=alpha_opt, random_state=seed
+    )  # normalize_components=True
+    spca.fit(cluster_features)
+
+    active_terms = np.nonzero(spca.components_[0])[0]
+    if len(active_terms) > 0:
+        spca_model[i, active_terms] = 1  # Set the active terms to 1
+
+# Plot the active terms in each cluster
+pf.plot_active_terms(spca_model, labels, False)
+
+
+# ---------------------------------------------
+# Resulting Final Balance Models
+# ---------------------------------------------
+
+print("----------------------------------")
+print("Obtaining Final balance models")
+print("----------------------------------")
+
+# Identify clusters with identical balance models
+balance_models, model_index = np.unique(spca_model, axis=0, return_inverse=True)
+nmodels = balance_models.shape[0]
+
+# Make new cluster_idx based on the unique SPCA balance model
+balance_idx = np.array([model_index[i] for i in cluster_idx])
+balancemap = np.reshape(balance_idx, [ny, nx], order="F")
+
+# Plot a grid with active terms in each cluster
+gridmap = balance_models.copy()
+gridmask = gridmap == 0
+gridmap = (
+    gridmap.T * np.arange(nmodels)
+).T + 1  # Scale map so that active terms can be color-coded
+gridmap[gridmask] = 0
+
+# Remove terms that are never used
+grid_mask = np.nonzero(np.all(gridmap == 0, axis=0))[0]
+gridmap = np.delete(gridmap, grid_mask, axis=1)
+grid_labels = np.delete(labels, grid_mask)
+
+# Plot the balance models in a grid
+pf.plot_balance_models(gridmap, grid_labels, False)
+
+# Plot the clustering in space after SPCA
+pf.plot_spca_reduced_clustering(x, y, balancemap, False)
+
+# Visualize the clusters in equation space with 2D projections
+pf.plot_feature_space(features[mask, :], balance_idx[mask], False)
+
+# ---------------------------------------------
+# Validate the balance models with some diagnostics
+# ---------------------------------------------
+print("----------------------------------")
+print("Validating the balance models")
+print("----------------------------------")
+
+# ----- Outer Layer Scaling -----
+# The length scale of the outer layer should scale with: l ~ x^(4/5)
+print("----- Outer layer scaling -----")
+
+u_map = np.reshape(u, (ny, nx), order="F")
+
+x_min = 0  # Where inertial balance begins
+x_turb = 600  # Where transitional region ends
+
+x_idx = np.nonzero(x > x_min)[0]
+x_layer = x[x_idx]
+
+# First, find the upper extent of the inertial sublayer
+y_gmm = np.zeros(len(x_idx))
+# Loop through wall-normal direction until the balance changes
+for i in range(len(x_idx)):
+    j = len(y) - 1
+    while balancemap[j, x_idx[i]] == 2:
+        j -= 1
+    y_gmm[i] = y[j]  # Store upper value of inertial balance
+
+# Next, find the 99% of free stream velocity line
+delta = np.zeros(len(x))
+# Loop until velocity falls past 99% freestream
+for i in range(len(x)):
+    j = 0
+    while u_map[j, i] < 0.99:
+        j += 1
+    delta[i] = y[j - 1]
+
+# Fit inertial balance to power law
+power_law = lambda x, a, b: a * x**b  # noqa: E731
+
+x_to_fit = x_layer > x_turb  # End of transitional region
+p_gmm, cov = curve_fit(power_law, x_layer[x_to_fit], y_gmm[x_to_fit])
+gmm_fit = power_law(x_layer, *p_gmm)
+print("Fitted parameters for the power law:")
+print(p_gmm)  # Print the fit parameters
+
+# Plot the inertial sublayer scaling
+pf.plot_sublayer_scaling(
+    x, y, balancemap, delta, x_layer, gmm_fit, p_gmm, x_to_fit, False
+)
+
+# ----- Self-similarity -----
+# In the near-wall region, the wall-normal profiles of velocity should be self-similar
+# over all x locations. Here we find that the identified vertical extent of the viscous
+# sublayer is too shallow compared to the "true" vertical extent of the sublayer
+# (based on the flow data).
+print("----- Self-similarity test -----")
+
+# Compute friction velocity with an estimate of the wall shear stress
+u_tau = np.sqrt(nu * uy[::ny])
+
+# Define wall units
+y_plus = np.outer(y, u_tau / nu)
+u_plus = np.reshape(u, [ny, nx], order="F") / u_tau
+
+# Plot the self-similarity of the flow
+print("y+ coordinates where the balance ends:")
+pf.plot_self_similarity(x, 1, y_plus, u_plus, balancemap, show=False)
