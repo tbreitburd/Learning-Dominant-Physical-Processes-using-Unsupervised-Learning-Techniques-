a2e02908bd46816a0f6ef42ab6cd601f5afff7e7
Thomas Breitburd <tmb76@cam.ac.uk>
2024-05-18 11:48:32 +0100

    Corrected some comments

--- Tools/preprocessing.py
+++ Tools/preprocessing.py
@@ -26,22 +26,24 @@ def get_derivatives(nx, ny, dx, dy):
 
     Dy = sparse.diags([-1, 1], [-1, 1], shape=(ny, ny)).toarray()
 
-    # Get 2nd order forward/backwards at the boundaries
+    # Get 2nd order accurate forward/backwards difference at the boundaries
     Dy[0, :3] = np.array([-3, 4, -1])
     Dy[-1, -3:] = np.array([1, -4, 3])
 
+    # Divide by 2*dy to get the derivative
     for i in range(ny - 1):
         Dy[i, :] = Dy[i, :] / (2 * dy[i])
     Dy[-1, :] = Dy[-1, :] / (2 * dy[-1])
 
-    # Repeat this for the x-direction
+    # Repeat the matrix for each points in the x-direction
     Dy = sparse.block_diag([Dy for i in range(nx)])  # Creates a block diagonal matrix
     # with the Dy matrix on the diagonal
 
+    # Get the 1st derivative with respect to x
     Dx = sparse.diags([-1, 1], [-ny, ny], shape=(nx * ny, nx * ny))
     Dx = sparse.lil_matrix(Dx)
 
-    # Get 2nd order forwards/backwards with boundary conditions
+    # Get 2nd order accurate forwards/backwards difference at boundaries
     for i in range(ny):
         Dx[i, i] = -3
         Dx[i, ny + i] = 4
@@ -51,7 +53,7 @@ def get_derivatives(nx, ny, dx, dy):
         Dx[-(i + 1), -(2 * ny + i + 1)] = 1
     Dx = Dx / (2 * dx)
 
-    Dx = sparse.csr_matrix(Dx)
-    Dy = sparse.csr_matrix(Dy)
+    Dx = Dx.tocsr()
+    Dy = Dy.tocsr()
 
     return Dx, Dy
