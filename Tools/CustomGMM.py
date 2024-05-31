797991762eeefb26d5d8999b38f9bbdfb82c600f
Thomas Breitburd <tmb76@cam.ac.uk>
2024-05-31 10:51:24 +0100

    Added documentation and error traps for __init__
    Checked n_components and n_features are at least 1. Checked for argument types and values.

--- Tools/CustomGMM.py
+++ Tools/CustomGMM.py
@@ -1,12 +1,65 @@
+"""!@file CustomGMM.py
+
+@brief This file contains the CustomGMM class, which is a custom implementation
+of the Gaussian Mixture Model clustering algorithm.
+
+@details The CustomGMM class is a custom implementation of the Gaussian Mixture
+Model clustering algorithm. The class is designed to be similar to the
+GaussianMixture class from the scikit-learn library, following the same initialisation,
+fitting, and prediction methods. The class is designed to be used as an alternative
+for the GaussianMixture class, with the same parameters and methods. It was written
+following the tutorial by Michael Galarnyk on Towards Data Science,
+(https://towardsdatascience.com/how-to-code-gaussian-
+mixture-models-from-scratch-in-python-9e7975df5252)
+and the documentation for the GaussianMixture class from scikit-learn
+(https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html).
+
+@author Created by T.Breitburd on 29/05/2023
+"""
 import numpy as np
 from scipy.stats import multivariate_normal
 from sklearn.cluster import KMeans
 
-# https://towardsdatascience.com/how-to-code-gaussian-mixture-models-from-scratch-in-python-9e7975df5252
-# https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html
-
 
 class CustomGMM:
+    """!@brief Custom implementation of the Gaussian Mixture Model clustering algorithm.
+
+    @details The CustomGMM class is a custom implementation of the Gaussian Mixture
+    Model clustering algorithm. The class is designed to be similar to the
+    GaussianMixture class from the sc
+    ikit-learn library, following the same initialisation,
+    fitting, and prediction methods. The class is designed
+    to be used as an alternative for the GaussianMixture class,
+    with the same parameters and methods. It was written
+    following the tutorial by Michael Galarnyk on Towards Data Science,
+    (https://towardsdatascience.com/how-to-code-gaussian-
+    mixture-models-from-scratch-in-python-9e7975df5252)
+    and the documentation for the GaussianMixture class from scikit-learn
+    (https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html).
+
+    @param n_components The number of components in the mixture model
+    @param n_features The number of features in the data
+    @param max_iter The maximum number of iterations for the EM algorithm
+    @param tolerance The tolerance for the convergence of the EM algorithm
+    @param random_state The random seed for the algorithm
+
+    @return A CustomGMM object
+
+    @attribute n_components The number of components in the mixture model,
+    i.e. the number of clusters
+    @attribute n_features The number of features in the data
+    @attribute max_iter The maximum number of iterations for the EM algorithm
+    @attribute tolerance The tolerance for the convergence of the EM algorithm
+    @attribute random_state The random seed for the algorithm
+    @attribute weights The weights of each gaussian component in the mixture model
+    @attribute means The means of each gaussian component in the mixture model
+    @attribute covariances The covariances of each gaussian component in the mixture model
+
+    @method fit Fit the Gaussian Mixture Model to the data using the E-M algorithm
+    @method predict Predict the cluster a certain sample in the same feature space belongs to.
+    """
+
+    # Initialise the CustomGMM object
     def __init__(
         self,
         n_components: int,
@@ -24,6 +77,37 @@ class CustomGMM:
         # Set the seeds
         np.random.seed(self.random_state)
 
+        # Check the parameters
+        if self.n_components < 1:
+            raise ValueError(
+                "Arg n_components must be at least 1, but has been given:",
+                self.n_components,
+            )
+        if not isinstance(self.n_components, int):
+            raise TypeError(
+                "n_components must be an integer, but is type:", type(self.n_components)
+            )
+        if self.n_features < 1:
+            raise ValueError(
+                "n_features must be at least 1-dimensional but has been given:",
+                self.n_features,
+            )
+        if self.max_iter < 1:
+            raise ValueError(
+                "max_iter must be at least 1, but has been given:", self.max_iter
+            )
+        if self.tolerance <= 0:
+            raise ValueError(
+                "tolerance must be at greater than 0, but has been given:",
+                self.tolerance,
+            )
+        if not isinstance(self.random_state, (int, type(None))):
+            raise TypeError(
+                "random_state must be an integer or None, but is type:",
+                type(self.random_state),
+            )
+
+    # Initialise the parameters
     def _initialise_parameters(self, features):
         # Initialise the weights as uniform
         self.weights = np.ones(self.n_components) / self.n_components
