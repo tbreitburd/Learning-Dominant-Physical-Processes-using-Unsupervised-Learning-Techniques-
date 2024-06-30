5db84fed0bef6629b050824158c4032494be1547
Thomas Breitburd <tmb76@cam.ac.uk>
2024-06-30 22:37:13 +0100

    Added docs for tools directory

---
+++ Tools/__init__.py
@@ -0,0 +1,17 @@
+"""!@module Tools
+
+@details This module contains all the tools and functions used in the project. The tools are
+organized into submodules for clarity and ease of use.
+The submodules are:
+- `blasius_solution`: A short module containing functions to compute the blaisus solution.
+- `CustomGMM`: A custom Gaussian Mixture Model class to use in the turbulent boundary layer case
+in order to test the reproducibility of the results.
+- `plot_funcs`: Contains all the plotting functions used in the project, with a few exceptions
+- `preprocessing`: Contains functions for the 2 different methods used to compute the
+derivatives terms in the Turbulent Boundary Layer case study.
+- `stability_assessment`: Contains functions that together implement the dominant balance for the
+stability assessment of the method.
+
+
+@author T.Breitburd and Callaham et al.
+"""
