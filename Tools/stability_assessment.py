e3dd727557ed2b2eedb347444dfbbe241046fe9b
Thomas Breitburd <tmb76@cam.ac.uk>
2024-06-30 14:30:58 +0100

    Added docs, modified code with custom functions
    Added docs for every function.
    
    Changed some of the code to be the alternative methods written in the BL notebook, also parallelisign the get_spca_residuals function

--- Tools/stability_assessment.py
+++ Tools/stability_assessment.py
@@ -1,27 +1,24 @@
 import numpy as np
 from sklearn.mixture import GaussianMixture
 from sklearn.decomposition import SparsePCA
+from joblib import Parallel, delayed
 
 np.random.seed(75016)
 
 
 def get_clusters(n_clusters, features, train_frac):
     """
-    Cluster the points in the equation space using a Gaussian Mixture Model.
+    !@brief Cluster the points in equation space using a Gaussian Mixture Model.
 
-    Params
-    ----------
-    n_clusters : The number of clusters to use, int
-    features : The features to cluster, np.array
-    train_frac : The fraction of the data to train on, float
+    @param n_clusters: The number of clusters, int
+    @param features: The equation space data with each term being a feature, np.array
+    @param train_frac: The fraction of the data to train on, float
 
-    Returns
-    -------
-    cluster_idx : The cluster index for each point, np.array
+    @return cluster_idx: The cluster index for each point, np.array
     """
     model = GaussianMixture(n_components=n_clusters, random_state=75016)
 
-    # Train on only a subset (10%) of the data
+    # Train on only a subset train_frac of the data
     sample_pct = train_frac
     mask = np.random.permutation(features.shape[0])[
         : int(sample_pct * features.shape[0])
@@ -33,43 +30,44 @@ def get_clusters(n_clusters, features, train_frac):
     return cluster_idx
 
 
-def get_spca_residuals(alphas, n_clusters, cluster_idx, eq_space_data, n_features):
+def get_spca_residuals(alphas, n_clusters, cluster_idx, eq_space_data):
     """
-    Calculate the SPCA residuals for each cluster, for each alpha value.
+    !@brief Calculate the error for each alpha value using Sparse PCA.
 
-    Params
-    ----------
-    alphas : The alpha values to test, list
-    n_clusters : The number of clusters, int
-    cluster_idx : The cluster index for each point, np.array
-    eq_space_data : The data with each term being a feature, np.array
-    n_features : The number of features, int
+    @param alphas: The alpha values to test, np.array
+    @param n_clusters: The number of clusters, int
+    @param cluster_idx: The cluster index for each point, np.array
+    @param eq_space_data: The data with each term being a feature, np.array
 
-    Returns
-    -------
-    err : The error for each alpha, np.array
+    @return err: The error for each alpha value, np.array
     """
 
     err = np.zeros([len(alphas)])
 
-    for k in range(len(alphas)):
-        for i in range(n_clusters):
+    def spca_err(alpha, cluster_idx, features, nc):
+        err_ = 0
+
+        for i in range(nc):
             # Identify points in the field corresponding to each cluster
-            feature_idx = np.nonzero(cluster_idx == i)[0]
-            cluster_features = eq_space_data[feature_idx, :]
+            feature_idx = np.where(cluster_idx == i)[0]
+            cluster_features = features[feature_idx, :]
 
             # Conduct Sparse PCA
-            spca = SparsePCA(n_components=1, alpha=alphas[k], random_state=75016)
+            spca = SparsePCA(n_components=1, alpha=alpha)
             spca.fit(cluster_features)
 
-            # Identify active and terms
-            active_terms = np.nonzero(spca.components_[0])[0]
-            inactive_terms = [
-                feat for feat in range(n_features) if feat not in active_terms
-            ]
+            # Identify active and inactive terms
+            inactive_terms = np.where(spca.components_[0] == 0)[0]
 
             # Calculate the error, as the sum of the norms of the inactive terms
-            err[k] += np.linalg.norm(cluster_features[:, inactive_terms])
+            err_ += np.sqrt(np.sum((cluster_features[:, inactive_terms].ravel()) ** 2))
+
+        return err_
+
+    err = Parallel(n_jobs=4)(
+        delayed(spca_err)(alpha, cluster_idx, eq_space_data, n_clusters)
+        for alpha in alphas
+    )
 
     return err
 
@@ -98,46 +96,34 @@ def get_spca_active_terms(
     )  # Store the active terms for each cluster
 
     for i in range(n_clusters):
-        feature_idx = np.nonzero(cluster_idx == i)[0]
+        feature_idx = np.where(cluster_idx == i)[0]
         cluster_features = eq_space_data[feature_idx, :]
 
-        spca = SparsePCA(
-            n_components=1, alpha=alpha_opt, random_state=75016
-        )  # normalize_components=True
+        # Apply Sparse PCA
+        spca = SparsePCA(n_components=1, alpha=alpha_opt)  # normalize_components=True
         spca.fit(cluster_features)
 
-        active_terms = np.nonzero(spca.components_[0])[0]
+        # Identify active terms
+        active_terms = np.where(spca.components_[0] != 0)[0]
         if len(active_terms) > 0:
             spca_model[i, active_terms] = 1  # Set the active terms to 1
 
     return spca_model
 
 
-def get_unique_balance_models(spca_model):
+def get_unique_balance_models(spca_model, labels):
     """
-    Get the unique balance models and the gridmap for the balance models.
+    !@brief Get the unique balance models and the mapping to the original model.
 
-    Params
-    ----------
-    spca_model : The active terms for each cluster, np.array
+    @param spca_model: The active terms for each cluster, np.array
+    @param labels: The labels for each term, np.array
 
-    Returns
-    -------
-    balance_models : The unique balance models, np.array
-    model_index : The index of the balance model for each cluster, np.array
-    nmodels : The number of unique balance models, int
-    gridmap : The gridmap for the balance models, np.array
-    grid_labels : The labels for the balance models, np.array
+    @return balance_models: The unique balance models, np.array
+    @return model_index: The mapping to the original model, np.array
+    @return nmodels: The number of unique models, int
+    @return gridmap: The mapping of the unique models to the original model, np.array
+    @return grid_labels: The labels for each term in the unique models, np.array
     """
-
-    labels = [
-        r"$\bar{u} \bar{u}_x$",
-        r"$\bar{v}\bar{u}_y$",
-        r"$\rho^{-1} \bar{p}_x$",
-        r"$\nu \nabla^2 \bar{u}$",
-        r"$\overline{(u^\prime v^\prime)}_y$",
-        r"$\overline{({u^\prime} ^2)}_x$",
-    ]
 
     balance_models, model_index = np.unique(spca_model, axis=0, return_inverse=True)
     nmodels = balance_models.shape[0]
